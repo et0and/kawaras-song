daaf3d226067b4d2f5fa77857bd5ee03b97bbf34
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-07-12 21:18:23 +1200

    In

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import { toWords } from "number-to-words";
+
+const Home: FC = () => {
+  const numbers = Array.from({ length: 100 }, (_, i) => i + 1);
+
+  return (
+    <main className="container mx-auto py-8 leading-10 text-center">
+      {numbers.map((num) => (
+        <p key={num} className="mb-1">
+          {toWords(num)}
+        </p>
+      ))}
+    </main>
+  );
+};
+
+export default Home;
